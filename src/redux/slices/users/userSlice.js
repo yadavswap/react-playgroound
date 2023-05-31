806a9746b7959ce643df25066286582e961c0cf8
Swapnil Yadav <46942236+yadavswap@users.noreply.github.com>
2023-05-31 18:07:38 +0530

    Users Initial State

---
+++ src/redux/slices/users/userSlice.js
@@ -0,0 +1,13 @@
+// initialState
+const initialState = {
+    loading:false,
+    error:null,
+    users:[],
+    user:{},
+    profile:{},
+    userAuth:{
+        loading:false,
+        error:null,
+        userInfo:{},
+    }
+}
