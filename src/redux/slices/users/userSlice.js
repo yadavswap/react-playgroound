d161462d8a4cfacffcd61933defa8ec29eecbe5d
Swapnil Yadav <46942236+yadavswap@users.noreply.github.com>
2023-06-01 15:05:11 +0530

    Login Action

--- src/redux/slices/users/userSlice.js
+++ src/redux/slices/users/userSlice.js
@@ -1,3 +1,6 @@
+import { createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit"
+import axios from "axios";
+import baseURL from "../../../utils/baseURL";
 // initialState
 const initialState = {
     loading:false,
@@ -11,3 +14,20 @@ const initialState = {
         userInfo:{},
     }
 }
+
+// login action
+export const loginUserAction = createAsyncThunk(
+    "users/login",
+    async ({email,password},{isRejectedWithValue,getState,dispatch})=>{
+        try {
+            // make the http request
+            const {data} = await axios.post(`${baseURL}/users/login`,{
+                email,
+                password,
+            });
+            return data;
+        } catch (error) {
+            return isRejectedWithValue(error?.response?.data);
+        }
+    }
+)
